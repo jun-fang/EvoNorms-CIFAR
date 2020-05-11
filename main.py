386d9430c8162829c9a33d7fd7e2d3cc5777648d
dinrker <junf2@uci.edu>
2020-05-10 23:48:32 -0700

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 import random
 import shutil
 import time
@@ -419,6 +420,35 @@ class ProgressMeter(object):
         num_digits = len(str(num_batches // 1))
         fmt = '{:' + str(num_digits) + 'd}'
         return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+
+# http://stackoverflow.com/questions/34950201/pycharm-print-end-r-statement-not-working
+class Logger(object):
+    def __init__(self):
+        self.terminal = sys.stdout  #stdout
+        self.file = None
+
+    def open(self, file, mode=None):
+        if mode is None: mode ='w'
+        self.file = open(file, mode)
+
+    def write(self, message, is_terminal=1, is_file=1 ):
+        if '\r' in message: is_file=0
+
+        if is_terminal == 1:
+            self.terminal.write(message)
+            self.terminal.flush()
+            #time.sleep(1)
+
+        if is_file == 1:
+            self.file.write(message)
+            self.file.flush()
+
+    def flush(self):
+        # this flush method is needed for python 3 compatibility.
+        # this handles the flush command by doing nothing.
+        # you might want to specify some extra behavior here.
+        pass
 
 
 def adjust_learning_rate(optimizer, epoch, args):
